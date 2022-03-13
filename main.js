2411b4523fba0130a6193321b303be150bd73cf4
joanotinho <joanbaixauli2004@gmail.com>
2022-03-13 18:55:31 +0100

    validate.js done

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 import {menuButton} from "./scripts/menuButton.js";
 import{plusMinusButton} from "./scripts/plusMinusButton.js";
 import{filter} from "./scripts/filter.js";
+import{formValidation} from "./scripts/formValidation.js";
 
 menuButton();
 
 plusMinusButton();
 
 filter();
+
+formValidation();
+
+
+
+
