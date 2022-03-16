41032a1f60a5f620ec3c76aa2c6d3fa4b04c94cf
joanotinho <joanbaixauli2004@gmail.com>
2022-03-16 20:51:31 +0100

    tabs done & formData done? definitive

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import {filter} from "./scripts/filter.js";
 import {formValidation} from "./scripts/formValidation.js";
 import {closeElement} from "./scripts/closeElement.js";
 import {formData} from "./scripts/formData.js"
+import {productDescription} from "./scripts/productDescription.js"
 
 menuButton();
 
@@ -14,5 +15,7 @@ filter();
 formValidation();
 
 closeElement();
+
+productDescription();
 
 formData();
